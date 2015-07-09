7e0aade8590a4f5566d9bcd504b8eadcaee23dab
randyvillanueva <streetfighterboy99@hotmail.com>
2015-07-08 18:11:47 -0700

    fixing typo, added spec for suffix 'II'

--- spec/lib/full-name-splitter_spec.rb
+++ spec/lib/full-name-splitter_spec.rb
@@ -28,6 +28,7 @@ describe Incognito do
 
     {
       "John Smith"                    => ["John",     nil,        "Smith"              ,nil   ,nil, nil   ],
+      "Kevin Smith II"                => ["Kevin",    nil,        "Smith"              ,nil,   "II", nil  ],
       "Kevin J. O'Connor"             => ["Kevin",    "J.",      "O'Connor"            ,nil   ,nil, 'J'   ],
       "Gabriel Van Helsing"           => ["Gabriel",  nil,      "Van Helsing"          ,nil   ,nil, nil   ],
       "Pierre de Montesquiou"         => ["Pierre",   nil,      "de Montesquiou"       ,nil   ,nil, nil   ],
